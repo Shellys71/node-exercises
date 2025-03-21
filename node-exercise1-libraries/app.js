798e005136c4f3f18ccb91f6a055a7fb5dda23f4
Shelly <shelysaraev@gmail.com>
2025-03-21 20:53:11 +0200

    Jokes exercise final version

---
+++ node-exercise1-libraries/app.js
@@ -0,0 +1,75 @@
+const yargs = require('yargs')
+const jokes = require('./jokes.js')
+
+// Costumize yargs version
+yargs.version('1.1.0')
+
+// Add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new joke',
+    handler() {
+        jokes.addJoke()
+    }
+})
+
+// Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a joke',
+    builder: {
+        name: {
+            describe: 'Name of the writer',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        jokes.removeJoke(argv.name)
+    }
+})
+
+// List command
+yargs.command({
+    command: 'list',
+    describe: 'List the jokes',
+    handler() {
+        jokes.listJokes()
+    }
+})
+
+// Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read the jokes',
+    builder: {
+        name: {
+            describe: 'Name of the writer',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        jokes.readJoke(argv.name)
+    }
+})
+
+// Add id command
+yargs.command({
+    command: 'id',
+    describe: 'Add id to a writer',
+    builder: {
+        name: {
+            describe: 'Name of the writer',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        jokes.addId(argv.name)
+    }
+})
+
+
+yargs.parse()
+
---
+++ node-exercise1-libraries/jokes.js
@@ -0,0 +1,102 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const giveMeAJoke = require('give-me-a-joke')
+const chance = require('chance').Chance()
+
+const addJoke = () => {
+    const jokes = loadJokes()
+    
+    giveMeAJoke.getRandomDadJoke (function(joke) {         
+
+        const duplicteJoke = jokes.find((joke) => joke.joke === joke)
+
+        if (!duplicteJoke) {
+            jokes.push ({
+                name: chance.name(),
+                age: chance.age(),
+                joke: joke
+            })
+            saveJokes(jokes)
+            console.log(chalk.green.inverse('New joke added!'))
+        } else {
+            console.log(chalk.red.inverse('Joke taken!'))
+        }
+    });
+}
+
+const removeJoke = (name) => {
+    const jokes = loadJokes()
+    const jokesToKeep = jokes.filter((joke) => joke.name !== name)
+
+    if (jokes.length > jokesToKeep.length) {
+        console.log(chalk.green.inverse('Joke removed!'))
+        saveJokes(jokesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No joke found!'))
+    }
+}
+
+const listJokes = () => {
+    const jokes = loadJokes()
+
+    console.log(chalk.underline.bold.magenta('Your jokes'))
+
+    jokes.forEach(joke => {
+        console.log(joke.joke)
+    });
+}
+
+const readJoke = (name) => {
+    const jokes = loadJokes()
+
+    const jokeToRead = jokes.find((joke) => joke.name === name)
+
+    if(jokeToRead) {
+        console.log(chalk.cyan.underline(jokeToRead.name))
+        console.log(jokeToRead.joke)
+    } else {
+        console.log(chalk.red.inverse('No joke found!'))
+    }
+}
+
+const addId = (name) => {
+    const jokes = loadJokes()
+
+    const writerToAddId = jokes.find((joke) => joke.name === name)
+
+    if(writerToAddId) {
+        if (!writerToAddId.id) {
+            writerToAddId.id = chance.ssn({ dashes: false })
+            console.log(writerToAddId)
+            saveJokes(jokes)
+            console.log(chalk.green.inverse('Id added!'))
+        } else {
+            console.log(chalk.green.inverse('This writer already has id!'))
+        }
+    } else {
+        console.log(chalk.red.inverse('No writer found!'))
+    }
+}
+
+const saveJokes = (jokes) => {
+    const dataJSON = JSON.stringify(jokes)
+    fs.writeFileSync('jokes.json', dataJSON)
+}
+
+const loadJokes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('jokes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    addJoke: addJoke,
+    removeJoke: removeJoke,
+    listJokes: listJokes,
+    readJoke: readJoke,
+    addId: addId
+}
